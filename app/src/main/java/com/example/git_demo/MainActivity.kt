3d620050cad7b8c46f222196d59a71d52d0ee05c
白潇 <xiao.bai@BKJK.com>
2022-06-29 19:37:24 +0800

    提交tv_one textview view

--- app/src/main/java/com/example/git_demo/MainActivity.kt
+++ app/src/main/java/com/example/git_demo/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.git_demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val mhandler : Handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Thread{
+            Thread.sleep(2000)
+            mhandler.post {
+                tv_one.setText("小白糖")
+            }
+        }.start()
     }
 }
